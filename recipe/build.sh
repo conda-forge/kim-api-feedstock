c247c958390aca9c6b44a1b17627196e4a3a86ff
Jan Janssen <jan-janssen@users.noreply.github.com>
2019-11-08 07:28:36 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,5 @@
 tar Jxf ${PKG_NAME}-${PKG_VERSION}.txz
 cd ${PKG_NAME}-${PKG_VERSION}
-patch -u -p1 -i ../diff.patch
 mkdir build && cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib -DCMAKE_BUILD_TYPE=Release -DKIM_API_SYSTEM_MODEL_DRIVERS_DIR=${PREFIX}/lib/openkim-models/model-drivers -DKIM_API_SYSTEM_PORTABLE_MODELS_DIR=${PREFIX}/lib/openkim-models/portable-models -DKIM_API_SYSTEM_SIMULATOR_MODELS_DIR=${PREFIX}/lib/openkim-models/simulator-models
 make
